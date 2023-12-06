9d8feed1adc010911a3af1d7a78480b4af10e117
archloner <filipbiernat@gmail.com>
2023-12-06 20:15:44 +0100

    Trying to deploy to netlify

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -16,23 +16,23 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-router.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+// router.get("/", function (req, res) {
+//   res.sendFile(__dirname + '/views/index.html');
+// });
 
 
 // your first API endpoint... 
-router.get("/api/hello", function (req, res) {
+router.get("/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
 
-router.get('/api/', function (req, res) {
+router.get('/', function (req, res) {
   let now = new Date();
   res.json({'unix': now.valueOf(), 'utc': now.toUTCString()})
 })
 
-router.get('/api/:time', function (req, res) {
+router.get('/:time', function (req, res) {
   let date = new Date(req.params.time)
   if (isNaN(date)) {
     date = new Date(parseInt(req.params.time))
@@ -50,6 +50,6 @@ var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-app.use('/', router);
+app.use('/api', router);
 
 export const handler = serverless(app); 
